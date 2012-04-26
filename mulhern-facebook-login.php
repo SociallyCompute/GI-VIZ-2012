d15b9777d7c8dc140ee7f4e31caed136df967372
Andrew Mulhern <ajm329@drexel.edu>
2012-04-25 22:45:10 -0400

    Adding mulhern-facebook-login.php per Activity 3.6

--- mulhern-facebook-login.php
+++ mulhern-facebook-login.php
@@ -13,8 +13,11 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations
  * under the License.
+ *
+ *
  * Modified by: Andrew Mulhern, INFO 655
-
+ *
+ *
  */
 
 require '../src/facebook.php';
